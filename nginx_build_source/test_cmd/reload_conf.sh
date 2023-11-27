855b58f5d1a20a26688d8640c4344e30fd719ac5
Maks <cnegbyj99@mail.ru>
2023-11-27 21:56:10 +0300

    lalala

---
+++ nginx_build_source/test_cmd/reload_conf.sh
@@ -0,0 +1,6 @@
+#!/bin/bash
+
+docker cp /home/makism/Myfolder/Scince/Docker_ngx/nginx_build_source/nginx_config/nginx.conf c2c15a4e2dde:/etc/nginx/nginx.conf
+
+
+docker exec -it c2c15a4e2dde bash -c "nginx -s reload"
---
+++ nginx_proxy/test_cmd/nginx_docker_reload.sh
@@ -0,0 +1,4 @@
+#!/bin/bash
+
+docker exec -it c2c15a4e2dde bash -c "nginx -s reload"
+
